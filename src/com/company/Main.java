655a2f54a17367f73fc5c2cc96bd6e429521ec2b
cm921 <sig91308@gmail.com>
2017-01-30 14:30:32 -0500

    Dang Got a plan though

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -4,6 +4,20 @@ import java.util.*;
 public class Main {
 
     public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        boolean looper = true;
+        int phoneumber = 0;
+        System.out.println("Hello user 3454");
+        while(looper){
+            System.out.println("Please input a ten digit phone number by which you would like to text");
+            try{
+                phoneumber= sc.nextInt();
+            }catch(InputMismatchException e){
+                System.out.println("Invalid input");
+                throw e;
+            }
+            System.out.println(phoneumber);
 
+        }
     }
 }
