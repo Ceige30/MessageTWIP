bc54f610c4cd0355adcf3d44977edf27b8899ed0
cm921 <sig91308@gmail.com>
2017-01-30 14:13:08 -0500

    First thing tester

---
+++ src/com/company/INFOANDSUCH.java
@@ -0,0 +1,10 @@
+package com.company;
+
+public class INFOANDSUCH {
+    /*
+    Plan of attack:
+    Array that contains the message
+    in the message class the array is change to contain the time before the actual message itself
+
+     */
+}
---
+++ src/com/company/MessageList.java
@@ -0,0 +1,7 @@
+package com.company;
+
+/**
+ * Created by cm921 on 1/30/17.
+ */
+public class MessageList {
+}
